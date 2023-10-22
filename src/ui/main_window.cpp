426eaaffd349f06e6174880df14401b13a7a5257
AnthonyCooper <126155805+AnthonyCooper1@users.noreply.github.com>
2023-10-22 22:40:56 +0300

    Create first shader triangle

--- src/ui/main_window.cpp
+++ src/ui/main_window.cpp
@@ -3,10 +3,43 @@
 #include "../utils.hpp"
 #include "../logger.hpp"
 #include "../libs/glad/include/glad/glad.h"
+#include "GLFW/glfw3.h"
 
 namespace nc {
 
+    GLfloat point[] = {
+        0.0f,   0.5f,   0.0f,
+        0.5f,   -0.5f,  0.0f,
+        -0.5f,  -0.5f,  0.0f
+    }; 
+    GLfloat color[] = {
+        1.0f, 0.0f, 0.0f,
+        0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 1.0f
+    };
     
+    const char* vertex_shader =
+        "#version 460\n"
+        "layout(location = 0) in vec3 vertex_position;"
+        "layout(location = 1) in vec3 vertex_color;"
+        "out vec3 color;"
+        "void main() {"
+        "   color = vertex_color;"
+        "   gl_Position = vec4(vertex_position, 1.0);"
+        "}";
+
+    const char* fragment_shader =
+        "#version 460\n"
+        "in vec3 color;"
+        "out vec4 frag_color;"
+        "void main() {"
+        "   frag_color = vec4(color, 1.0);"
+        "}";
+
+    GLuint shader_program;
+    GLuint vao;
+    
+
     int MainWindow::init()
     {
         
@@ -63,14 +96,58 @@ namespace nc {
         //io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
         //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesJapanese());
         //IM_ASSERT(font != nullptr);
+        glfwSetFramebufferSizeCallback(_glWindow, [](GLFWwindow* w, int width, int height)
+                                       {
+                                            glViewport(0, 0, width, height);
+                                       });    
 
+
+        GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vs, 1, &vertex_shader, nullptr);
+        glCompileShader(vs);
+
+        GLuint fs = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fs, 1, &fragment_shader, nullptr);
+        glCompileShader(fs);
+
+        shader_program = glCreateProgram();
+        glAttachShader(shader_program, vs);
+        glAttachShader(shader_program, fs);
+        glLinkProgram(shader_program);
+
+        glDeleteShader(vs);
+        glDeleteShader(fs);
+
+        GLuint points_vbo = 0;
+        glGenBuffers(1, &points_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(point), point, GL_STATIC_DRAW);
+        GLuint colors_vbo = 0;
+        glGenBuffers(1, &colors_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(color), color, GL_STATIC_DRAW);
+    
+        glGenVertexArrays(1, &vao);
+        glBindVertexArray(vao);
+        
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+        glEnableVertexAttribArray(1);
+        glBindBuffer(GL_ARRAY_BUFFER, colors_vbo);
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
         return 0; 
     }
     
     void MainWindow::update()
     {
-        
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shader_program);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         // Poll and handle events (inputs, window resize, etc.)
         // You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
         // - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
@@ -93,10 +170,10 @@ namespace nc {
         // Rendering
         ImGui::Render();
         int display_w, display_h;
-        glfwGetFramebufferSize(_glWindow, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0,0.5,0.5,1);
-        glClear(GL_COLOR_BUFFER_BIT);
+        // glfwGetFramebufferSize(_glWindow, &display_w, &display_h);
+        // glViewport(0, 0, display_w, display_h);
+        // glClearColor(0,0.5,0.5,1);
+        // glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
         glfwSwapBuffers(_glWindow);
