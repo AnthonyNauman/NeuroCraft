a8534f71b2c1d1283c6159aab125dce664e62125
biolegger <philasaf@gmail.com>
2023-12-02 16:35:06 +0300

    general changes

--- src/app.cpp
+++ src/app.cpp
@@ -12,9 +12,8 @@ namespace nc {
     int App::exec()
     {
 
-        _mainWindow = std::make_unique<MainWindow>();
         std::string projName = std::string(PROJECT_NAME) + "_" + std::string(PROJECT_VERSION);
-        _mainWindow.reset(new MainWindow(1600, 800, projName));
+        _mainWindow = std::make_unique<MainWindow>(1600, 800, projName);
         int errorCode = _mainWindow->init();
         if(errorCode) {
             NC_LOG_ERROR("Cant init main window!");
